8bb17b28fec1772f82f823ebb44952c1a9cac8b0
MarinaCIN <mgb@cin.ufpe.br>
2016-10-14 16:03:20 -0300

    Update script.bat

--- Projeto1/script.bat
+++ Projeto1/script.bat
@@ -1,3 +1,5 @@
+SET CLASSPATH=.;C:\Users\mgb\workspace\Compiladores\Projeto1\lib\antlr-4.5.3-complete.jar;%CLASSPATH%
+SET PATH=.;C:\Program Files\Java\jdk1.8.0_91\bin\;%PATH%
 java org.antlr.v4.Tool Projeto.g4
 javac Projeto*.java
 java org.antlr.v4.runtime.misc.TestRig Projeto ini -gui input.txt
