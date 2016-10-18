5ae5bd180719f2937a9a9dcef3740eaab8b88f8d
MarinaCIN <mgb@cin.ufpe.br>
2016-10-18 15:33:57 -0300

    Script corrigido

--- Projeto1/script.bat
+++ Projeto1/script.bat
@@ -1,3 +1,5 @@
+SET CLASSPATH=.;C:\Users\mgb\Desktop\Projeto\lib\antlr-4.5.3-complete.jar;%CLASSPATH%
+SET PATH=.;C:\Program Files\Java\jdk1.8.0_65\bin\;%PATH%
 java org.antlr.v4.Tool Projeto.g4
 javac Projeto*.java
 java org.antlr.v4.runtime.misc.TestRig Projeto pgm -gui input.txt
